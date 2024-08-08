fca921173e7e8f18e01fced743cc01fdcac6de08
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 08:31:55 -0400

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(500, 500);
+  background(220, 220, 220);
+	let r = random(0,255);
+	let g = random(0,255);
+	let b = random(0,255);
+	stroke(r,g,b);
+}
+
+
+
+
+function draw() {
+	
+
+	line(250,250,mouseX,mouseY)
+	
+
+	//Dont know how to not make it rainbow 
+}
+
+function mouseClicked() {
+  
+}
+
+
+
+
+
+
+
